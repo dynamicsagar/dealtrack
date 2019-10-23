e5859509b15e8d0bb6b1f3fd04a730d9f8acf8e6
dynamicsagar <dynamicsagar@gmail.com>
2019-10-23 17:38:26 +0530

    added newly test cases to test suite

--- testcases/external_counsel/external_counsel_testcases.py
+++ testcases/external_counsel/external_counsel_testcases.py
@@ -6,7 +6,7 @@ import logging
 
 
 @pytest.mark.usefixtures("oneTimeSetUp", "setUp")
-class LoginTests(unittest.TestCase):
+class ExternalCounselTests(unittest.TestCase):
     log = cl.customLogger(logging.DEBUG)
 
     @pytest.fixture(autouse=True)
